1e703dc5443cebda81619b5781c533f372cf7cf0
Jonathan Granda <jhonatangranda.2002@gmail.com>
2025-04-08 19:48:26 -0500

    Implementación parcial de la subida de archivos

---
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/FileController.java
@@ -0,0 +1,45 @@
+package com.proyIntUdeA.proyectoIntegradorI.controller;
+
+import com.proyIntUdeA.proyectoIntegradorI.entity.FileEntity;
+import com.proyIntUdeA.proyectoIntegradorI.response.ResponseFile;
+import com.proyIntUdeA.proyectoIntegradorI.response.ResponseMessage;
+import com.proyIntUdeA.proyectoIntegradorI.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/fileManager")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        fileService.store(file);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage("Archivo subido de manera HD"));
+    }
+
+    @GetMapping("/files/{id}")
+    public ResponseEntity<byte[]> getFile(@PathVariable UUID id) throws FileNotFoundException {
+        FileEntity fileEntity = fileService.getFile(id).get();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment: filename\"" +fileEntity.getName()+"\"")
+                .body(fileEntity.getData());
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<List<ResponseFile>> getListFiles(){
+        List<ResponseFile> files = fileService.getAllFiles();
+        return ResponseEntity.status(HttpStatus.OK).body(files);
+    }
+}
