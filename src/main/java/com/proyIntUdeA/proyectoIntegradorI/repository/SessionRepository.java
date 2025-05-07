d3fd187b56d733816a64cb7d5cb24113dc499872
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-05-07 16:46:58 -0500

    Se agrega la consulta requerida para basicTutoringInfo

--- src/main/java/com/proyIntUdeA/proyectoIntegradorI/repository/SessionRepository.java
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/repository/SessionRepository.java
@@ -2,9 +2,19 @@ package com.proyIntUdeA.proyectoIntegradorI.repository;
 
 import com.proyIntUdeA.proyectoIntegradorI.entity.SessionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface SessionRepository extends JpaRepository<SessionEntity, Long> {
 
+    @Query(value = "SELECT c.class_date, s.subject_name, c.class_state, t.user_id as tutor_id, t.user_firstname as tutor_name, t.user_lastname as tutor_lastname  " +
+            "FROM class c" +
+            "JOIN subject s ON s.subject_id = c.subject_id " +
+            "JOIN person t ON t.user_id = c.tutor_id", nativeQuery = true)
+    List<Map<String, Object>> findBasicTutoInfo(@Param("studentId") String studentId);
 }
