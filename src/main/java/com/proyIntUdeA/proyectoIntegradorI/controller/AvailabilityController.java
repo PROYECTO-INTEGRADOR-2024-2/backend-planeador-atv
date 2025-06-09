a0ea64e1d682d35f218645fa8a39e409f23d6091
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-06-08 21:11:27 -0500

    Se agrega la funcionalidad para filtrar también por tutorías asignadas

--- src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/AvailabilityController.java
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/AvailabilityController.java
@@ -3,19 +3,23 @@ package com.proyIntUdeA.proyectoIntegradorI.controller;
 import com.proyIntUdeA.proyectoIntegradorI.dto.TutorSearchDTO;
 import com.proyIntUdeA.proyectoIntegradorI.entity.SlotAvailabilityEntity;
 import com.proyIntUdeA.proyectoIntegradorI.model.ListTutorsByTimeRequest;
+import com.proyIntUdeA.proyectoIntegradorI.service.SessionService;
 import com.proyIntUdeA.proyectoIntegradorI.service.SubjectTutorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class AvailabilityController {
     private final SubjectTutorService subjectTutorService;
+    private final SessionService sessionService;
 
-    public AvailabilityController(SubjectTutorService subjectTutorService) {
+    public AvailabilityController(SubjectTutorService subjectTutorService, SessionService sessionService) {
         this.subjectTutorService = subjectTutorService;
+        this.sessionService = sessionService;
     }
 
     @PostMapping("/listarTutores")
@@ -23,7 +27,12 @@ public class AvailabilityController {
         Long subjectId = listTutorsByTimeRequest.getSubjectId();
         String hour = listTutorsByTimeRequest.getHour();
         String day = listTutorsByTimeRequest.getDayWeek();
-        List<TutorSearchDTO> tutors = subjectTutorService.findByTime(subjectId,hour, day );
+        String period = listTutorsByTimeRequest.getPeriod();
+        String date = listTutorsByTimeRequest.getDate();
+        List<TutorSearchDTO> tutors = subjectTutorService.findByTime(subjectId,hour, day ).stream().filter(
+                tutor -> !sessionService.verificarDispoTutor(tutor.getTutorId(), date, hour, period)
+        ).collect(Collectors.toList());
+
         return ResponseEntity.ok(tutors);
     }
 }
