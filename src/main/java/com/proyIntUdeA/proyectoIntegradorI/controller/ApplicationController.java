1173a1ce23c663859401a1eeb8d78ee5a914b46e
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-07-03 21:13:15 -0500

    Se agrega la validación de token al endpoint

--- src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/ApplicationController.java
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/ApplicationController.java
@@ -1,13 +1,18 @@
 package com.proyIntUdeA.proyectoIntegradorI.controller;
 
+import com.proyIntUdeA.proyectoIntegradorI.Jwt.JwtService;
+import com.proyIntUdeA.proyectoIntegradorI.dto.BasicTutoringInfoAdminDTO;
 import com.proyIntUdeA.proyectoIntegradorI.dto.TutorActivationRequest;
 import com.proyIntUdeA.proyectoIntegradorI.entity.ApplicationActivationTutorEntity;
 import com.proyIntUdeA.proyectoIntegradorI.model.ApplicationActivationTutor;
+import com.proyIntUdeA.proyectoIntegradorI.model.Session;
 import com.proyIntUdeA.proyectoIntegradorI.model.UserSubjectRequest;
 import com.proyIntUdeA.proyectoIntegradorI.model.UserXSubject;
 import com.proyIntUdeA.proyectoIntegradorI.service.ActivationService;
 import com.proyIntUdeA.proyectoIntegradorI.service.SubjectTutorService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +25,7 @@ import java.util.stream.Collectors;
 public class ApplicationController {
     private ActivationService activationService;
     private SubjectTutorService subjectTutorService;
+    private JwtService jwtService;
     
     public ApplicationController(ActivationService activationService, SubjectTutorService subjectTutorService) {
         this.activationService = activationService;
@@ -32,8 +38,24 @@ public class ApplicationController {
     }
 
     @GetMapping("/application")
-    public List<TutorActivationRequest> getAllApplications() {
-        return activationService.listActivations();
+    public ResponseEntity<?> getAllApplications(HttpServletRequest request) {
+
+        ResponseEntity<String> tokenVerification = jwtService.verifyToken(request);
+
+        if (tokenVerification.getStatusCode() != HttpStatus.OK) {
+            return tokenVerification;
+        }
+
+        String token = request.getHeader("Authorization").substring(7);
+        String userRole = jwtService.getClaim(token, claims -> claims.get("user_role", String.class));
+        if(!userRole.equalsIgnoreCase("ROLE_ADMIN")){
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Debes ser usuario administrador");
+        }
+
+        List<TutorActivationRequest> info = activationService.listActivations();
+        return ResponseEntity.ok(info);
     }
 
     @PutMapping("/application/accept/{id}")
