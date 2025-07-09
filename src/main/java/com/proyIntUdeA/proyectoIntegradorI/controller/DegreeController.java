827583e6c498252fdef225a09cc570672af14b71
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-07-09 10:00:09 -0500

    Se agrega validación de token a agregar carreras

--- src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/DegreeController.java
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/DegreeController.java
@@ -2,6 +2,8 @@ package com.proyIntUdeA.proyectoIntegradorI.controller;
 
 import com.proyIntUdeA.proyectoIntegradorI.model.Degree;
 import com.proyIntUdeA.proyectoIntegradorI.service.DegreeService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,18 +11,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.proyIntUdeA.proyectoIntegradorI.Jwt.JwtService;
+
 @RestController
 @RequestMapping("/api/v1/")
 public class DegreeController {
     private DegreeService degreeService;
+    private JwtService jwtService;
 
-    public DegreeController(DegreeService degreeService) {
+    public DegreeController(JwtService jwtService, DegreeService degreeService) {
+        this.jwtService = jwtService;
         this.degreeService = degreeService;
     }
 
     @PostMapping("/degree")
-    public Degree saveDegree(@RequestBody Degree degree) {
-        return degreeService.saveDegree(degree);
+    public ResponseEntity<?> saveDegree(@RequestBody Degree degree, HttpServletRequest request) {
+        ResponseEntity<String> tokenVerification = jwtService.verifyToken(request);
+
+        if (tokenVerification.getStatusCode() != HttpStatus.OK) {
+            return tokenVerification;
+        }
+
+        String token = request.getHeader("Authorization").substring(7);
+        String user_role = jwtService.getClaim(token, claims -> claims.get("user_role", String.class)).toLowerCase();
+
+        if(!user_role.equalsIgnoreCase("ROLE_ADMIN")){
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Debes ser usuario administrador");
+        }
+
+        Degree response = degreeService.saveDegree(degree);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/degree")
