1beec95e1b21eab1867227d331a5188f856ba9fb
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-07-08 20:07:14 -0500

    Se arregla el endpoint para listar tutores

--- src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/AvailabilityController.java
+++ src/main/java/com/proyIntUdeA/proyectoIntegradorI/controller/AvailabilityController.java
@@ -4,8 +4,11 @@ import com.proyIntUdeA.proyectoIntegradorI.dto.TutorSearchDTO;
 import com.proyIntUdeA.proyectoIntegradorI.model.ListTutorsByTimeRequest;
 import com.proyIntUdeA.proyectoIntegradorI.service.SessionService;
 import com.proyIntUdeA.proyectoIntegradorI.service.SubjectTutorService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.proyIntUdeA.proyectoIntegradorI.Jwt.JwtService;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,14 +19,26 @@ import java.util.stream.Collectors;
 public class AvailabilityController {
     private final SubjectTutorService subjectTutorService;
     private final SessionService sessionService;
+    private final JwtService jwtService;
 
-    public AvailabilityController(SubjectTutorService subjectTutorService, SessionService sessionService) {
+    public AvailabilityController(SubjectTutorService subjectTutorService, SessionService sessionService, JwtService jwtService) {
         this.subjectTutorService = subjectTutorService;
         this.sessionService = sessionService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/listarTutores")
-    public ResponseEntity<List<TutorSearchDTO>> findTutorsByTime(@RequestBody ListTutorsByTimeRequest listTutorsByTimeRequest) {
+    public ResponseEntity<?> findTutorsByTime(@RequestBody ListTutorsByTimeRequest listTutorsByTimeRequest, HttpServletRequest request) {
+        ResponseEntity<String> tokenVerification = jwtService.verifyToken(request);
+
+        if (tokenVerification.getStatusCode() != HttpStatus.OK) {
+            return tokenVerification;
+        }
+
+        String token = request.getHeader("Authorization").substring(7);
+        String tutorId = jwtService.getClaim(token, claims -> claims.get("user_id", String.class));
+
+
         Long subjectId = listTutorsByTimeRequest.getSubjectId();
         String hour = listTutorsByTimeRequest.getHour();
         String day = listTutorsByTimeRequest.getDayWeek();
@@ -32,6 +47,7 @@ public class AvailabilityController {
 
         List<TutorSearchDTO> tutors = subjectTutorService.findByTime(subjectId, hour, day, period).stream()
                 .filter(tutor -> !sessionService.verificarDispoTutor(tutor.getTutorId(), date, hour, period))
+                .filter(tutor -> !tutor.getTutorId().equals(tutorId))
                 .collect(Collectors.toList());
 
         // Crear y agregar el tutor por defecto con ID "0"
@@ -44,6 +60,8 @@ public class AvailabilityController {
         if (!alreadyIncluded) {
             tutors.add(defaultTutor);
         }
+
+
 
         return ResponseEntity.ok(tutors);
     }
